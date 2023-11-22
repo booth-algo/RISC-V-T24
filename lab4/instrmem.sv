c241ab892870bbee66b2089e20b0cd37a397dca8
noamweitz <weitzmanaom@gmail.com>
2023-11-22 14:38:35 +0000

    change in size of ImmSrc

--- lab4/instrmem.sv
+++ lab4/instrmem.sv
@@ -2,13 +2,13 @@ module instrmem #(
     parameter ADDRESS_WIDTH = 32,
     DATA_WIDTH = 32
 ) (
-    input logic [ADDRESS_WIDTH-1:0] pc,
-    output logic [DATA_WIDTH-1:0] instr
+    input logic [ADDRESS_WIDTH-1:0] A,
+    output logic [DATA_WIDTH-1:0] RD
 );
 
 logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
 
 always_comb //non-synchronous
-    instr <= rom_array[pc]; //the word address is increased by 4, but this should be implemented in the PCreg
+    RD <= rom_array[A]; //the word address is increased by 4, but this should be implemented in the PCreg
     
 endmodule
--- lab4/signextend.sv
+++ lab4/signextend.sv
@@ -2,15 +2,15 @@ module signextend #(
     parameter DATA_WIDTH = 32
 ) (
     input logic [DATA_WIDTH-1:0] instr,
-    input logic ImmSrc,
+    input logic [1:0] ImmSrc,
     output logic [DATA_WIDTH-1:0] ImmOp
 );
 
 always_comb //non-synchronous
-    if (ImmSrc)
+    if (ImmSrc == 0'b00)
         ImmOp = {{DATA_WIDTH-12{instr[31]}}, instr[31:20]};
     else
-    ImmOp = {{DATA_WIDTH-12{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+        ImmOp = {{DATA_WIDTH-12{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
     
 endmodule
 
