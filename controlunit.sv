bf5e02a68f6e1a9d676ecabadfaff2690a844111
noamweitz <weitzmanaom@gmail.com>
2023-11-22 10:57:14 +0000

    draft 4 for the control unit

--- controlunit.sv
+++ controlunit.sv
@@ -1,43 +1,51 @@
 module controlunit #(
     parameter DATA_WIDTH = 32
 ) (
-   input logic [6:0] op,
-   input logic EQ, //not sure what this is for
-   output logic ALUctrl, //this missing
-   output logic ALUsrc, //ok
-   output logic [1:0] ImmSrc, //ok
-   output logic PCsrc, //this missing
-   output logic RegWrite //ok
+   input logic [DATA_WIDTH-1:0] instr,
+   input logic EQ,
+   output logic ALUctrl,
+   output logic ALUsrc,
+   output logic [1:0] ImmSrc,
+   output logic PCsrc,
+   output logic RegWrite
 );
 
+logic [6:0] op;
+logic [2:0] funct3;
+
 always_comb begin
+    op <= instr[6:0];
+    funct3 <= instr[14:12];
+
     case(op)
-        7'b0000011: begin           //lw
-            RegWrite = 1;
-            ImmSrc = 2'b00;
-            ALUSrc = 1;
-            ALUOp = 2'b00;
+        7'b0010011: begin
+            case(funct3)
+                3'b000: begin        //addi operation
+                    PCsrc = 0;
+                    ImmSrc = 2'b00;
+                    ALUSrc = 1;
+                    ALUctrl = 0;
+                    RegWrite = 1;
+                end
+
+                default: begin
+                    jnec;
+                end
         end
 
-        7'b0100011: begin           //sw
-            RegWrite = 0;
-            ImmSrc = 2'b01;
-            ALUSrc = 1;
-            ALUOp = 2'b00;
-        end
+        7'b1100011: begin
+            case(funct3)
+                3'b001: begin       //bne operation
+                    PCsrc = 1;
+                    ImmSrc = 2'b10;
+                    ALUSrc = 0;
+                    ALUctrl = 1;
+                    RegWrite = 0;
+                end
 
-        7'b0110011: begin           //R-type
-            RegWrite = 1;
-            //ImmSrc = 2'b00;
-            ALUSrc = 0;
-            ALUOp = 2'b10;
-        end
-
-        7'b1100011: begin           //beq
-            RegWrite = 0;
-            ImmSrc = 2'b10;
-            ALUSrc = 0;
-            ALUOp = 2'b01;
+                default: begin
+                    ine;
+                end
         end
 
         default: begin
