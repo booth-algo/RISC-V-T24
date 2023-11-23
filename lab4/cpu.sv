e9d3bf97a01a9404d0463f8fbb88c4c0902b9359
booth-algo <kvl01@protonmail.com>
2023-11-23 10:03:39 +0000

    Fixed instr signal

--- lab4/cpu.sv
+++ lab4/cpu.sv
@@ -19,9 +19,9 @@ module cpu #(
     logic [WIDTH-1:0] PC;
 
     // Regfile
-    logic rs1;
-    logic rs2;
-    logic rd;
+    logic rs1 = instr[19:15];
+    logic rs2 = instr[24:20];
+    logic rd = instr[11:7];
     logic RegWrite;
 
     // Sign Extend
@@ -40,18 +40,15 @@ module cpu #(
     
     alu alu_inst (
         .ALUop1(ALUop1),
-        // ALUop2 should not be an input based on your design
         .ALUctrl(ALUctrl),
-        // should not have a clk
         .regOp2(regOp2),
         .ImmOp(ImmOp),
         .ALUsrc(ALUsrc),
         .EQ(EQ),
-        .ALUout(ALUout) // should technically be SUM, not ALUout, but it's fine, we can fix it later
+        .ALUout(ALUout) 
     );
 
     reg_file reg_file_inst (
-        // your input and output in your module declaration of "regfile" is inaccurate - you should define by portname, not signal name
         .AD1(rs1),
         .AD2(rs2),
         .AD3(rd),
@@ -62,7 +59,6 @@ module cpu #(
     );
 
     instr_mem instr_mem_inst (
-        // similar issue - port name declaration is inaccurate
         .A(PC),
         .RD(instr)
     );
@@ -79,8 +75,8 @@ module cpu #(
 
     sign_extend sign_extend_inst (
         .ImmOp(ImmOp),
-        .ImmOpSrc(ImmSrc),
-        .instr(instr)
+        .ImmSrc(ImmSrc),
+        .instr(instr[31:7])
     );
 
 endmodule
