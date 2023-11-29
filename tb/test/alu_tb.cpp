0cea511e35104321376305dabd497215e85fdaa1
noamweitz <weitzmanaom@gmail.com>
2023-11-29 12:28:20 +0000

    modified version of the ALU testbench by Noam

--- tb/test/alu_tb.cpp
+++ tb/test/alu_tb.cpp
@@ -8,6 +8,9 @@
 #define NAME            "alu"
 #define OPCODE_ADD      0b000
 #define OPCODE_SUB      0b001
+#define OPCODE_AND      0b010
+#define OPCODE_OR       0b011
+#define OPCODE_SLT      0b111
 
 class ALUTestbench : public BaseTestbench
 {
@@ -25,35 +28,92 @@ protected:
 
 TEST_F(ALUTestbench, AdditionTest)
 {
+    op1 = 5;
+    op2 = 10;
     //inputs for addition operation
-    top->ALUop1 = 5;
+    top->ALUop1 = op1;
     top->ALUctrl = OPCODE_ADD;
-    top->regOp2 = 10;
+    top->regOp2 = op2;
     top->ImmOp = 0;
     top->ALUsrc = 0;
 
     top->eval();
 
     //check the ALU output and EQ signal for addition
-    EXPECT_EQ(top->ALUout, 15);
-    EXPECT_EQ(top->EQ, 0);
+    EXPECT_EQ(top->ALUout, op1 + op2);
+    EXPECT_EQ(top->EQ, op1 + op2 == 0);
 }
-
 
 TEST_F(ALUTestbench, SubtractionTest)
 {
+    op1 = 5;
+    op2 = 5;
     //inputs for subtraction operation
-    top->ALUop1 = 5;
+    top->ALUop1 = op1;
     top->ALUctrl = OPCODE_SUB;
-    top->regOp2 = 5;
+    top->regOp2 = op2;
     top->ImmOp = 0;
     top->ALUsrc = 0;
 
     top->eval();
 
     //check the ALU output and EQ signal for subtraction
-    EXPECT_EQ(top->ALUout, 0);
-    EXPECT_EQ(top->EQ, 1);
+    EXPECT_EQ(top->ALUout, op1 - op2);
+    EXPECT_EQ(top->EQ, op1 - op2 == 0);
+}
+
+TEST_F(ALUTestbench, BinaryAndTest)
+{
+    op1 = 0b0110;
+    op2 = 0b0101;
+    //inputs for binary AND operation
+    top->ALUop1 = op1;
+    top->ALUctrl = OPCODE_AND;
+    top->regOp2 = op2;
+    top->ImmOp = 0;
+    top->ALUsrc = 0;
+
+    top->eval();
+
+    //check the ALU output and EQ signal for AND
+    EXPECT_EQ(top->ALUout, op1 & op2);
+    EXPECT_EQ(top->EQ, (op1 & op2) == 0);
+}
+
+TEST_F(ALUTestbench, BinaryOrTest)
+{
+    op1 = 0b0110;
+    op2 = 0b0101;
+    //inputs for binary OR operation
+    top->ALUop1 = op1;
+    top->ALUctrl = OPCODE_OR;
+    top->regOp2 = op2;
+    top->ImmOp = 0;
+    top->ALUsrc = 0;
+
+    top->eval();
+
+    //check the ALU output and EQ signal for OR
+    EXPECT_EQ(top->ALUout, op1 | op2);
+    EXPECT_EQ(top->EQ, (op1 | op2) == 0);
+}
+
+TEST_F(ALUTestbench, SetIfLessThanTest)
+{
+    op1 = 0b0110;
+    op2 = 0b0101;
+    //inputs for binary SLT operation
+    top->ALUop1 = op1;
+    top->ALUctrl = OPCODE_OR;
+    top->regOp2 = op2;
+    top->ImmOp = 0;
+    top->ALUsrc = 0;
+
+    top->eval();
+
+    //check the ALU output and EQ signal for SLT
+    EXPECT_EQ(top->ALUout, (op1 < op2) ? 1 : 0);
+    EXPECT_EQ(top->EQ, (op1 < op2) == 0);
 }
 
 int main(int argc, char **argv)
